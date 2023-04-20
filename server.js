685e345a1488b1b75ab16d50c23152c386f72f3b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-04-20 14:00:44 -0400

    Create a new server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
