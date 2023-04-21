2897bf86ae2069f28b7653d00ad9b400486ae143
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-04-21 03:20:10 -0400

    Add the pg database credentials

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5001;
+
+const { Client } = require('pg');
+const db = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+db.connect();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
